db13daa77bff24df89c4c342fed58c59adb9f2bb
Scott Isenberg <galdwulfram@gmail.com>
2018-01-02 14:38:35 -0600

    Release v0.0.1-0

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
         push: true,
-        pushTo :'origin',
+        pushTo :'upstream',
         gotDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
         globalReplace: false,
         prereleaseName: false,
