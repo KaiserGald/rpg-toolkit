0dc906273f076a8b5a943c43eacc74bd7966d33e
Scott Isenberg <galdwulfram@gmail.com>
2018-01-03 17:56:16 -0600

    Release v0.1.1-dev.1

--- rpgapp.go
+++ rpgapp.go
@@ -1,9 +1,24 @@
 package main
 
-func processFlags() {
+import (
+	"os"
 
+	"github.com/KaiserGald/rpgApp/daemon"
+	"github.com/KaiserGald/rpgApp/services/logger"
+)
+
+func processFlags() *daemon.Config {
+	cfg := &daemon.Config{}
+	cfg.ListenSpec = ":8080"
+	return cfg
 }
 
 func main() {
+	cfg := processFlags()
+	log := &logger.Logger{}
 
+	log.Init(os.Stdout, os.Stdout, os.Stderr)
+	if err := daemon.Run(cfg, log); err != nil {
+		log.Error.Printf("Error in main(): %v\n", err)
+	}
 }
