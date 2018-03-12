cc56daedc21efd54bd618c9f22724a7506b04785
Scott Isenberg <galdwulfram@gmail.com>
2018-03-12 12:36:42 -0500

    adds rest of unit tests

--- router/handler/handlers/404_test.go
+++ router/handler/handlers/404_test.go
@@ -4,3 +4,18 @@
 // © 2018 Scott Isenberg
 
 package fnftest
+
+import (
+	"testing"
+
+	baloo "gopkg.in/h2non/baloo.v3"
+)
+
+var test = baloo.New("http://localhost:8080")
+
+func TestBaloo404Handler(t *testing.T) {
+	test.Get("/bad").
+		Expect(t).
+		Status(404).
+		Done()
+}
